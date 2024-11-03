3667817365c146a9d41a04bf3a21f034b4921fd4
ZF <z@hysp.org>
2024-11-02 21:12:22 -0700

    refactor slack function into composite classes

--- botchan/slack/reaction.py
+++ botchan/slack/reaction.py
@@ -3,6 +3,8 @@
 All methods return a ReactionResponse.
 """
 
+from abc import ABC
+
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
@@ -13,19 +15,20 @@ from botchan.slack.exception import SlackResponseError
 logger = get_logger(__name__)
 
 
-def add_reaction(
-    client: WebClient, event: MessageEvent, reaction_name: str
-) -> ReactionResponse:
-    """
-    Adds a reaction to a message.
-    """
-    try:
-        response = client.reactions_add(
-            name=reaction_name, channel=event.channel, timestamp=event.ts
-        )
-        if response.status_code == 200 and response["ok"]:
-            return ReactionResponse.from_api_response(response)
-        else:
-            raise SlackResponseError(response)
-    except SlackApiError as e:
-        logger.error("Error adding reaction", error=str(e))
+class SlackReaction(ABC):
+    slack_client: WebClient
+
+    def add_reaction(self, event: MessageEvent, reaction_name: str) -> ReactionResponse:
+        """
+        Adds a reaction to a message.
+        """
+        try:
+            response = self.slack_client.reactions_add(
+                name=reaction_name, channel=event.channel, timestamp=event.ts
+            )
+            if response.status_code == 200 and response["ok"]:
+                return ReactionResponse.from_api_response(response)
+            else:
+                raise SlackResponseError(response)
+        except SlackApiError as e:
+            logger.error("Error adding reaction", error=str(e))
